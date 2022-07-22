938ad86e8e7ae7a37640fb9636b7d21aad3682de
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-22 21:40:52 +0200

    Update APP_love-calculator.py

--- APP_love-calculator.py
+++ APP_love-calculator.py
@@ -1,3 +1,20 @@
+# Take both people's names and check for the number of times the letters in the word TRUE occurs. 
+
+# Then check for the number of times the letters in the word LOVE occurs. 
+
+# Then combine these numbers to make a 2 digit number.
+
+# For Love Scores less than 10 or greater than 90, the message should be:
+
+# "Your score is **x**, you go together like coke and mentos."
+# For Love Scores between 40 and 50, the message should be:
+
+# "Your score is **y**, you are alright together."
+# Otherwise, the message will just be their score. e.g.:
+
+# "Your score is **z**."
+
+
 print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
